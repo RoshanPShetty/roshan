9f4c1ced13030c0b19f6c257bfaa03c87a58cdb5
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-13 09:17:47 -0400

    Implemented project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { FaHome } from "react-icons/fa";
 
@@ -18,6 +19,7 @@ export default function Home() {
 				/>
 				<Hero />
 				<Grid />
+				<RecentProjects />
 			</div>
 		</main>
 	);
