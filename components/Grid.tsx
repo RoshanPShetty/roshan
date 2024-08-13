7f1303d01b3f00cc8318bb70d056512be6916d55
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-13 08:23:04 -0400

    Added title to Bento Grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,6 +5,10 @@ import { gridItems } from "@/data";
 const Grid = () => {
 	return (
 		<section id="about">
+			<h1 className="heading">
+				An insight to{" "}
+				<span className="text-purple">who I am</span>
+			</h1>
 			<BentoGrid className="w-full py-20">
 				{gridItems.map(
 					({
