e64b076255646b980de131b48f2374efd18c827d
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-14 03:54:12 -0400

    Changed from GTM to Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,16 +20,16 @@ export default function RootLayout({
 		<html lang="en">
 			<head>
 				<Script
-					id="google-tag-manager"
-					strategy="afterInteractive"
-					dangerouslySetInnerHTML={{
-						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TPQ5VP56');`,
-					}}
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-9K4J3KC75D"
 				></Script>
+				<Script id="google-analytics">
+					{`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9K4J3KC75D');`}
+				</Script>
 			</head>
 			<body className={inter.className}>
 				<ThemeProvider
