140e35c3b8b7bc260ea348de0a7e952c033cdc89
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-14 00:35:43 -0400

    Footer implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -8,7 +9,7 @@ import { navItems } from "@/data";
 
 export default function Home() {
 	return (
-		<main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+		<main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
 			<div className="max-w-7xl w-full">
 				<FloatingNav navItems={navItems} />
 				<Hero />
@@ -16,6 +17,7 @@ export default function Home() {
 				<RecentProjects />
 				<References />
 				<Experience />
+				<Footer />
 			</div>
 		</main>
 	);
