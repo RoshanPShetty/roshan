d3d108265f636696f400656e36290f0eb40a603d
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-14 00:55:40 -0400

    Fixed bugs in navbar, header, and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,9 +5,6 @@ import { socialMedia } from "@/data";
 
 const Footer = () => {
     return <footer  className="w-full pt-20 pb-10" id="contact">
-        <div className="w-full absolute left-0 -bottom-72 min-h-96">
-            <img src="/footer-grid.svg" alt="grid" className="w-full h-full opacity-50"/>
-        </div>
         <div className="flex flex-col items-center">
             <h1 className="heading lg:max-w-[45vw]">Want to <span className="text-purple">collaborate</span> and build something?</h1>
             <p className="text-white-200 md:mt-10 my-5 text-center">Reach out to me today and let&apos;s connect to build something great.</p>
@@ -18,7 +15,7 @@ const Footer = () => {
 
         <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
             <p className="md:text-base text-sm md:font-normal font-light">Copyright © 2024 Roshan Praveen Shetty</p>
-            <div className="flex items-center md:gap-3 gap-6">
+            <div className="flex items-center md:gap-3 gap-6 mt-5">
                 {socialMedia.map((profile) => (
                     <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-150 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
                         <img src={profile.img} alt={profile.id.toString()} width={20} height={20}/>
